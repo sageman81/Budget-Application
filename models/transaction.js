dd55efc63aff0f016f3db2d4d41ef5ad2380f844
Sageman81 <sagemichael1@gmail.com>
2024-03-11 18:57:05 -0400

    posts, edits and deletes working

--- models/transaction.js
+++ models/transaction.js
@@ -4,10 +4,13 @@ const mongoose = require('mongoose');
 const transactionSchema = new mongoose.Schema({
     name: { type: String, required: true },
     amount: { type: Number, required: true },
-    category: String,
+    category: {
+      type: String,
+      required: true},
+      
     date: { type: Date, default: Date.now },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    
+
 });
 
 const Transaction = mongoose.model('Transaction', transactionSchema);
